0dd1e8580bbdbd6f74672dfff42339b91da75a70
Akshaya Parida <akshayaparida2811@gmail.com>
2025-02-19 23:00:07 +0530

    Set up EJS as the view engine and configure views directory
    - Added EJS as a dependency in package.json
    - Configured EJS as the view engine in index.ts
    - Set the views directory to ./views
    - Added scripts to watch and copy view files
    
    Benefits of using EJS:
    - Server-Side Rendering: Improves initial load time and SEO.
    - Dynamic Content: Allows embedding JavaScript code within HTML to generate dynamic content.
    - Easy to Use: Simple syntax and easy to learn.
    - Integration with Express: Seamless integration with Express for easy setup.

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,10 +1,17 @@
 var _a;
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get("/", (req, res) => {
-    return res.send("Hello World");
+    return res.render("welcome");
 });
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app: Application = express();
 const port = process.env.PORT ?? 8000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "./views"))
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("Hello World");
+  return res.render("welcome");
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
