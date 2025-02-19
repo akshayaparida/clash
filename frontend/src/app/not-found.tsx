14de8bd2555c56284cfe0c0612f83abd89dd87af
Akshaya Parida <akshayaparida2811@gmail.com>
2025-02-19 18:04:17 +0530

    add 404 error not found page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+    <div className='h-screen flex justify-center items-center flex-col'>
+     <Image src="/404Error.svg" width={500} height={500} alt="404 error"></Image>
       <Link href="/">Return Home</Link>
     </div>
   )
-}
+} 
